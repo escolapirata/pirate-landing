344ded3d9d9fbbd29d055a5cadc41863f016352f
mknarciso <murilokn@gmail.com>
2016-06-24 12:26:40 +0000

    merge trouble fix

--- config/routes.rb
+++ config/routes.rb
@@ -19,69 +19,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   post 'beta_testers/', to: 'beta_testers#create'
   get 'pages/:name', to: 'pages#search'
-  
-  
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-<<<<<<< HEAD
-end
-
-
-FacebookAuthExample::Application.routes.draw do
-  resources :blogposts
-=======
->>>>>>> bc66290509889e9b71e03cf07432fd4abaa31266
-  resources :certificates
- 
-  resources :paths
- 
   get 'auth/:provider/callback', to: 'sessions#create' 
  
   get 'auth/failure', to: redirect('/')
